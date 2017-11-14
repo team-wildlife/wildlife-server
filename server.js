4ca9c99038c0f18533628172e48c55160822c3e0
Catherine Looper <loopercat@gmail.com>
2017-11-14 14:08:09 -0800

    Added description route

--- server.js
+++ server.js
@@ -30,6 +30,17 @@ app.get('/api/v1/commonName/:name', (req, res) => {
     )
 });
 
+app.get('/api/v1/narrative/:name', (req, res) => {
+  const url = `http://apiv3.iucnredlist.org/api/v3/species/narrative/${req.params.name}?token=${process.env.REDLIST_TOKEN}`
+  console.log(url)
+  superagent.get(url)
+    .then(
+      speciesByCountry => res.send(speciesByCountry.text),
+      err => res.send(err)
+    )
+});
+
+
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
