9c5fcbf57196b1deac169c5b5820168d62f7f226
Catherine Looper <loopercat@gmail.com>
2017-11-14 11:39:07 -0800

    Set up get request for API call for common name

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ app.get('/api/v1/countries/:iso', (req, res) => {
     )
 });
 
+app.get('/api/v1/commonName/:name', (req, res) => {
+  const url = `http://apiv3.iucnredlist.org/api/v3/species/common_names/${req.params.name}?token=${process.env.REDLIST_TOKEN}`
+  console.log(url)
+  superagent.get(url)
+    .then(
+      speciesByCountry => res.send(speciesByCountry.text),
+      err => res.send(err)
+    )
+});
+
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
